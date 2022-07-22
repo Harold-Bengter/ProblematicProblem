23d235c7982042f21e1aedd2c8b0a4b5ccdee51d
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-22 14:52:07 -0400

    Made code more CCR

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -6,7 +6,7 @@ namespace ProblematicProblem
 {
     class Program
     {
-       
+
         static bool cont = true;
         static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
         static void Main(string[] args)
@@ -25,21 +25,22 @@ namespace ProblematicProblem
             {
                 cont = false;
             }
-            
+
 
             Console.WriteLine();
 
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
-            
+
             Console.WriteLine();
 
             Console.Write("What is your age? ");
             int userAge = int.Parse(Console.ReadLine());
 
             Console.WriteLine();
+
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = Console.ReadLine().ToLower() == "sure"? true : false;
+            bool seeList = Console.ReadLine().ToLower() == "sure" ? true : false;
 
             if (seeList)
             {
@@ -50,30 +51,37 @@ namespace ProblematicProblem
                 }
 
                 Console.WriteLine();
+
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                bool addToList = Console.ReadLine().ToLower() == "yes"? true : false;
+                bool addToList = Console.ReadLine().ToLower() == "yes" ? true : false;
+
                 Console.WriteLine();
 
                 while (addToList)
                 {
                     Console.Write("What would you like to add? ");
                     string userAddition = Console.ReadLine();
+
                     activities.Add(userAddition);
+
                     foreach (string activity in activities)
                     {
                         Console.Write($"{activity} ");
                         Thread.Sleep(250);
                     }
+
                     Console.WriteLine();
+
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    addToList = Console.ReadLine().ToLower() == "yes"? true : false;
+                    addToList = Console.ReadLine().ToLower() == "yes" ? true : false;
+
                     Console.WriteLine();
                 }
             }
-               while (cont)
+            while (cont)
             {
                 Console.Write("Connecting to the database");
-               for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 10; i++)
                 {
                     Console.Write(". ");
                     Thread.Sleep(500);
@@ -106,13 +114,15 @@ namespace ProblematicProblem
 
                     randomNumber = rng.Next(activities.Count);
 
-                    randomActivity = activities[randomNumber]; 
+                    randomActivity = activities[randomNumber];
 
                 }
 
 
                 Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+
                 Console.WriteLine();
+
                 cont = (Console.ReadLine().ToLower() == "redo") ? true : false;
             }
         }
